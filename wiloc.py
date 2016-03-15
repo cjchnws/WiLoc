98b82d8b500a83271ecc7bfa9f85707ab72125f5
Marc Hanheide <marc@hanheide.de>
2016-03-15 23:07:37 +0100

    some more helper functions

--- wiloc.py
+++ wiloc.py
@@ -5,13 +5,60 @@ from pymongo import MongoClient
 from json import load
 from datetime import datetime
 
+import cv2 as cv
 
 from dateutil.parser import parse
+
+def draw_location(img, position, scale=10):
+    s = img.shape
+    p = (int(s[0] / 2.0 + position[0] * scale),
+         int(s[1] / 2.0 + position[1] * scale))
+    #print p
+    if p[0] < 0:
+        return
+    if p[0] >= s[0]:
+        return
+    if p[1] < 0:
+        return
+    if p[1] >= s[1]:
+        return
+
+    cv.circle(img, p, 3, (0, 0, 255), -1)
+
+def draw_locations(img, data, scale=10):
+    c = 0
+    minim = np.min(data, axis=1)
+    maxim = np.max(data, axis=1)
+    print minim, maxim
+    for r in data.T:
+        draw_location(img, (r[0,0], r[0,1]), scale)
+        c += 1
+    print "drew circles: ", c
+
+
+cv.namedWindow('location')
+
+
+img = np.zeros([300, 300, 3])
+
+
+#data = np.mat([[0, 0],[10,-10],[10,0]]).T
+
+#draw_locations(img,data)
+
+#cv.imshow('location', img)
+
+#cv.waitKey(1)
+
 
 with open('data-aaf.json', 'r') as infile:
     j = load(infile)
 
 c = 0
+
+
+
+
 
 new_d = {}
 for k in j['data']:
@@ -75,8 +122,11 @@ print data.shape
 mu = np.mean(data, axis=1)
 data_c = data-mu
 
+draw_locations(img, data_c[0:2,:],2)
 
+cv.imshow('location', img)
 
+cv.waitKey(0)
 #C_full = (data_c*data_c.T) / n_points
 
 #C_P = C_full[0:2,0:2]
@@ -98,7 +148,7 @@ Pp = np.linalg.pinv(P)
 
 A = W * Pp
 
-query = np.mat([10, 10]).T - mu_P
+query = np.mat([100, 100]).T - mu_P
 
 R = (A * query) + mu_W
 
@@ -108,3 +158,5 @@ print R
 print np.sqrt(np.sum(D * D) / (n_points*n_bssids))
 
 
+
+cv.waitKey(0)
